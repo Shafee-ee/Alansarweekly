7cafdcf2220a84328beea73bbbb101ee9beac71c
shafee_azeem <shafeeghani@gmail.com>
2025-09-17 02:50:38 +0530

    First Commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+export default function App() {
+  return (
+    <div className="p-10 text-center">
+      <h1 className="text-4xl font-bold text-red-600">Tailwind is working!</h1>
+      <h1 className="text-4xl font-bold text-blue-200">Tailwind is working!</h1>
+      <h1 className="text-4xl font-bold text-blue-900">Tailwind is working!</h1>
+
+    </div>
+  )
+}
---
+++ src/main.jsx
@@ -0,0 +1,21 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+import Home from "./pages/Home";
+import About from "./pages/About";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      { path: "", element: <Home /> },
+      { path: "about", element: <About /> },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <RouterProvider router={router} />
+);
